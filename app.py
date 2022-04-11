c4f2638636aba50b449b139b3d5d1608fba20936
juanskysword <juanskysword@gmail.com>
2022-04-11 16:52:06 -0500

    webupdate

--- app.py
+++ app.py
@@ -14,6 +14,10 @@ def home():
 def store():
     return render_template('shop.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 @app.route("/signup", methods=["POST", "GET"])
 def signup():
     if request.method == "POST":
